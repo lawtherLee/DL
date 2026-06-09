bd3d3f6ed5e586484a67d117cb8e4bad5a8e2b98
leeSZ <108440505+lawtherLee@users.noreply.github.com>
2026-06-09 09:03:49 +0800

    尝试对图像分类案例进行优化

--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
-from sklearn.externals.array_api_compat import torch
+import torch
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
@@ -27,33 +27,41 @@ def create_dataset():
 class ImageModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(
-            3,
-            6,
-            3,
-        )
-        self.pool1 = nn.MaxPool2d(2, 2, 0)
-        self.conv2 = nn.Conv2d(
-            6,
-            16,
-            3,
-        )
-        self.pool2 = nn.MaxPool2d(2, 2, 0)
 
-        self.linear1 = nn.Linear(576, 120)
-        self.linear2 = nn.Linear(120, 84)
-        self.output = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(32, 128, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        self.linear1 = nn.Linear(128 * 8 * 8, 2048)
+        self.linear2 = nn.Linear(2048, 2048)
+        self.output = nn.Linear(2048, 10)
+
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
-        _x = self.pool2(torch.relu(self.conv2(self.pool1(torch.relu(self.conv1(x))))))
-        # print(x.shape)
-        # print(_x.shape)
-        # print(_x.reshape(_x.size(0), -1).shape)
-        return self.output(
-            torch.relu(
-                self.linear2(torch.relu(self.linear1(_x.reshape(_x.size(0), -1))))
-            )
-        )
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = torch.relu(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = torch.relu(x)
+        x = self.pool2(x)
+
+        x = x.reshape(x.size(0), -1)
+
+        x = torch.relu(self.linear1(x))
+        x = self.dropout(x)
+
+        x = torch.relu(self.linear2(x))
+        x = self.dropout(x)
+
+        return self.output(x)
 
 
 def train(train_dataset):
@@ -64,13 +72,14 @@ def train(train_dataset):
     )
     model = ImageModel()
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4, weight_decay=1e-4)
 
     epochs = 50
     for epoch in range(epochs):
         total_loss, total_samples, total_correct, start = 0, 0, 0, time.time()
+        model.train()
+
         for x, y in dataloader:
-            model.train()
             y_pred = model(x)
             loss = criterion(y_pred, y)
             optimizer.zero_grad()
@@ -91,11 +100,26 @@ def train(train_dataset):
 
 
 def evaluate(test_dataset):
-    pass
+    dataloader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+    model = ImageModel()
+    model.load_state_dict(torch.load("./model/image_model.pth"))
+    model.eval()
+    total_correct, total_samples = 0, 0
+    with torch.no_grad():
+        for x, y in dataloader:
+            y_pred = model(x)
+            total_correct += (torch.argmax(y_pred, dim=-1) == y).sum()
+            total_samples += len(y)
+
+    print(f"acc: {total_correct / total_samples:.2f}")
 
 
 if __name__ == "__main__":
-    batch_size = 8
+    batch_size = 128
     train_dataset, test_dataset = create_dataset()
     # print(f"训练集: {train_dataset.data.shape}")
     # model = ImageModel()
